ed012552f2350b6070b573cd827c843914e7c02c
sarah2234 <2234sarah@gmail.com>
2022-12-23 12:33:06 +0900

    feat: check if provided url is valid

---
+++ shortener_app/main.py
@@ -0,0 +1,13 @@
+import validators
+from fastapi import FastAPI, HTTPException
+
+from . import schemas
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided URL is not valid")
+    return f"TODO: Create database entry for: {url.target_url}"
